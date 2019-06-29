96d9f989a733eab328e9daa8c8171114ffd4b2c0
Jeannette <jeannetteshui.ng@gmail.com>
2019-06-29 02:13:38 -0400

    Updates

---
+++ precache-manifest.15819c63ab3c46749f42975be604f6b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12db5b5a63d7d38ecc4aa984df97843b",
+    "url": "/MealTime/index.html"
+  },
+  {
+    "revision": "f2e8b6bf3526a5684e44",
+    "url": "/MealTime/static/css/main.e1e42e4d.chunk.css"
+  },
+  {
+    "revision": "fec3bbee9061cfa01f30",
+    "url": "/MealTime/static/js/2.6acccac0.chunk.js"
+  },
+  {
+    "revision": "f2e8b6bf3526a5684e44",
+    "url": "/MealTime/static/js/main.a30a08e4.chunk.js"
+  },
+  {
+    "revision": "6e079b543795b7b8cdce",
+    "url": "/MealTime/static/js/runtime~main.1fd64be9.js"
+  },
+  {
+    "revision": "aab78f87d014d7d8c578ca17bdcf40ec",
+    "url": "/MealTime/static/media/Espresso-Lungo.aab78f87.mp4"
+  },
+  {
+    "revision": "0753d4a142fb38b8475b407941a07e30",
+    "url": "/MealTime/static/media/food.0753d4a1.jpg"
+  }
+]);
